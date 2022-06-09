7a2e8627089b81d61fc7edc733a5e8a1f7c10e7e
Simon Ochsenreither <simon@ochsenreither.de>
2022-06-09 19:25:06 +0200

    add turnback

--- assets/js/currentPage.js
+++
@@ -1,5 +0,0 @@
-let pageLinks = window.document.querySelectorAll(".page-link");
-pageLinks.forEach(link => {
-    if (new URL(link.href).pathname === window.location.pathname)
-        link.classList.add("current-page");
-});
